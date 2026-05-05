be9c04650e4d8591b863594467f56e0e1f4a86bf
Arda Katrancıoğlu <ardakatrancioglu@gmail.com>
2026-05-05 14:52:38 +0200

    added style checker

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+      globals: {
+        ...globals.node
+      }
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-undef': 'error',
+      'no-console': 'off',
+      semi: ['error', 'always'],
+      quotes: ['error', 'single'],
+      indent: ['error', 2],
+      'comma-dangle': ['error', 'never']
+    }
+  }
+];
